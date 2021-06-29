5a357cc4d766924ef57f67c740f34925e03c0b3f
kyo9150 <kyo9150@gmail.com>
2021-06-29 11:33:58 +0900

    アップロード

---
+++ insert.sql
@@ -0,0 +1,251 @@
+USE kabukidb;
+
+INSERT INTO mst_category(category_name,category_description) VALUES (
+'春の誕生石',
+'3,4,5月の誕生石'
+); 
+INSERT INTO mst_category (category_name,category_description) VALUES (
+'夏の誕生石',
+'6,7,8月の誕生石'
+);
+INSERT INTO mst_category(category_name,category_description) VALUES (
+'秋の誕生石',
+'9,10,11の誕生石'
+); 
+INSERT INTO mst_category (category_name,category_description) VALUES (
+'冬の誕生石',
+'12,1,2月の誕生石'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ガーネット',
+'がーねっと',
+'1月の誕生石、真実・友愛・忠実',
+4,
+45000,
+'/img/garnet.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'アメシスト',
+'あめしすと',
+'2月の誕生石、誠実・心の平和',
+4,
+40000,
+'/img/ame.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'アクアマリン',
+'あくあまりん',
+'3月の誕生石、沈着・勇敢 ・聡明',
+1,
+35000,
+'/img/aqua.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ブラッドストーン',
+'ぶらっどすとーん',
+'3月の誕生石、沈着・勇敢 ・聡明',
+1,
+2000,
+'/img/blood.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'サンゴ',
+'さんご',
+'3月の誕生石、沈着・勇敢 ・聡明',
+1,
+10000,
+'/img/sango.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ダイヤモンド',
+'だいやもんど',
+'4月の誕生石、清純無垢',
+1,
+100000,
+'/img/dia.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'エメラルド',
+'えめらるど',
+'5月の誕生石、幸運・幸福',
+1,
+90000,
+'/img/eme.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ヒスイ',
+'ひすい',
+'5月の誕生石、幸運・幸福',
+1,
+90000,
+'/img/jadeite.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ムーンストーン',
+'むーんすとーん',
+'6月の誕生石、健康・富・長寿',
+2,
+55000,
+'/img/moon.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'真珠',
+'しんじゅ',
+'6月の誕生石、健康・富・長寿',
+2,
+55000,
+'/img/pearl.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ルビー',
+'るびー',
+'7月の誕生石、熱情・仁愛・威厳',
+2,
+80000,
+'/img/ruby.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ペリドット',
+'ぺりどっと',
+'8月の誕生石、夫婦の幸福 ・和合',
+2,
+25000,
+'/img/olivine.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'サードニクス',
+'さーどにくす',
+'8月の誕生石、夫婦の幸福 ・和合',
+2,
+4000,
+'/img/sardonyx.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'サファイア',
+'さふぁいあ',
+'9月の誕生石、慈愛・誠実・徳望',
+3,
+75000,
+'/img/sapphire.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'オパール',
+'おぱーる',
+'10月の誕生石、歓喜・安楽・忍耐',
+3,
+60000,
+'/img/opal.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'トルマリン',
+'とるまりん',
+'10月の誕生石、歓喜・安楽・忍耐',
+3,
+60000,
+'/img/elbaite.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'トパーズ',
+'とぱーず',
+'11月の誕生石、友情・希望・潔白',
+3,
+30000,
+'/img/topaz.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'シトリン',
+'しとりん',
+'11月の誕生石、友情・希望・潔白',
+3,
+3000,
+'/img/citrine.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'トルコ石',
+'とるこせき',
+'12月の誕生石、成功',
+4,
+20000,
+'/img/turkey.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'ラピスラズリ',
+'らぴすらずり',
+'12月の誕生石、成功',
+4,
+20000,
+'/img/lazu.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+INSERT INTO mst_product (product_name,product_name_kana,product_description,category_id,price,image_full_path,release_date,release_company) VALUES (
+'タンザナイト',
+'たんざないと',
+'12月の誕生石、成功',
+4,
+60000,
+'/img/tanzanite.jpeg',
+'2021/4/1',
+'株式会社藤島'
+);
+
+
